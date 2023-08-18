0d67e9f367749bb1aaff22c566485edfd06cd685
kicap <kicap@kicaps-iMac.local>
2023-08-19 02:42:52 +0800

    modify api for edit jabatan

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -13,10 +13,10 @@ class Api extends RestController
 {
   function __construct()
   {
-      parent::__construct();
-      $this->load->model('model');;
-      // $this->db->query("SET sql_mode = '' ");
-      date_default_timezone_set("Asia/Kuala_Lumpur");
+    parent::__construct();
+    $this->load->model('model');;
+    // $this->db->query("SET sql_mode = '' ");
+    date_default_timezone_set("Asia/Kuala_Lumpur");
   }
 
   public function index_get()
@@ -33,11 +33,11 @@ class Api extends RestController
 
   }
 
-  
-  
+
+
 
   // -----------------------------------------------------------------------------------------------------------
-  
+
   public function siswa_post()
   {
     $nama = $this->post('nama');
@@ -57,7 +57,7 @@ class Api extends RestController
 
     $cek_last_ai = $this->model->cek_last_ai('tb_siswa');
 
-    $upload_dir = 'assets/siswa/'.$cek_last_ai.'/';
+    $upload_dir = 'assets/siswa/' . $cek_last_ai . '/';
     if (!is_dir($upload_dir)) {
       mkdir($upload_dir);
     }
@@ -81,20 +81,18 @@ class Api extends RestController
       'hobi' => $hobi,
       'img_url' => $path
     ];
-    
+
     $this->model->insert('tb_siswa', $array);
 
 
 
     $this->response(['message' => 'ini untuk siswa post', 'status' => $array], 200);
-
   }
 
   public function siswa_get()
   {
     $data = $this->model->tampil_data_keseluruhan('tb_siswa')->result();
-    $this->response(['message' => 'ini untuk siswa get', 'status' => true , 'data' => $data], 200);
-
+    $this->response(['message' => 'ini untuk siswa get', 'status' => true, 'data' => $data], 200);
   }
 
   public function siswa_detail_get()
@@ -102,8 +100,7 @@ class Api extends RestController
     $id = $this->get('id');
     $data = $this->model->tampil_data_where('tb_siswa', ['id_siswa' => $id])->result();
     if (count($data) == 0) return $this->response(['message' => 'data tidak ditemukan', 'status' => false], 200);
-    $this->response(['message' => 'ini untuk siswa get', 'status' => true , 'data' => $data[0]], 200);
-
+    $this->response(['message' => 'ini untuk siswa get', 'status' => true, 'data' => $data[0]], 200);
   }
 
 
@@ -126,14 +123,12 @@ class Api extends RestController
     $this->model->insert('tb_dana_sosial', $array);
 
     $this->response(['message' => 'ini untuk dana sosial', 'status' => true], 200);
-
   }
 
   public function dana_sosial_get()
   {
     $data = $this->model->tampil_data_keseluruhan('tb_dana_sosial')->result();
-    $this->response(['message' => 'ini untuk dana sosial get', 'status' => true , 'data' => $data], 200);
-
+    $this->response(['message' => 'ini untuk dana sosial get', 'status' => true, 'data' => $data], 200);
   }
 
   public function dana_sosial_detail_get()
@@ -141,17 +136,17 @@ class Api extends RestController
     $id = $this->get('id');
     $data = $this->model->tampil_data_where('tb_dana_sosial', ['id_dana_sosial' => $id])->result();
     if (count($data) == 0) return $this->response(['message' => 'data tidak ditemukan', 'status' => false], 400);
-    $this->response(['message' => 'ini untuk dana sosial get', 'status' => true , 'data' => $data[0]], 200);
-
+    $this->response(['message' => 'ini untuk dana sosial get', 'status' => true, 'data' => $data[0]], 200);
   }
 
 
-  public function filter_dana_post(){
+  public function filter_dana_post()
+  {
     $sql = $this->post('sql');
 
     $data = $this->model->custom_query($sql)->result();
 
-    $this->response(['message' => 'ini untuk filter dana', 'status' => true , 'data' => $data], 200);
+    $this->response(['message' => 'ini untuk filter dana', 'status' => true, 'data' => $data], 200);
   }
 
   public function dana_sosial_ttd_post()
@@ -164,8 +159,55 @@ class Api extends RestController
     $this->model->update('tb_dana_sosial', ['id_dana_sosial' => $id], ['status' => 1]);
 
     $this->response(['message' => 'ini untuk dana sosial ttd', 'status' => true], 200);
-
   }
-  
-}
 
+  public function edit_jabatan_post()
+  {
+    $jabatan = $this->post('jabatan');
+    $jumlah = $this->post('jumlah');
+    // change jumlah to int
+    $jumlah = (int)$jumlah;
+
+    $cek_data = $this->model->tampil_data_where('tb_jabatan', ['jabatan' => $jabatan])->result();
+    for ($j = 0; $j < count($cek_data); $j++) {
+      // delete folder
+      $upload_dir = 'assets/jabatan/' . $cek_data[$j]->id . '/';
+      if (is_dir($upload_dir)) {
+        $files = glob($upload_dir . '*');
+        foreach ($files as $file) {
+          if (is_file($file)) {
+            unlink($file);
+          }
+        }
+        rmdir($upload_dir);
+      }
+    }
+    $this->model->delete('tb_jabatan', ['jabatan' => $jabatan]);
+
+    for($i = 1; $i <= $jumlah; $i++){
+      $nama = $this->post('nama'.$i);
+      $foto = $_FILES['image'.$i];
+
+      $cek_last_ai = $this->model->cek_last_ai('tb_jabatan');
+
+      $upload_dir = 'assets/jabatan/' . $cek_last_ai . '/';
+      if (!is_dir($upload_dir)) {
+        mkdir($upload_dir);
+      }
+
+      $path = $upload_dir . $foto['name'];
+      move_uploaded_file($foto['tmp_name'], $path);
+
+      $array = [
+        'jabatan' => $jabatan,
+        'nama' => $nama,
+        'img_url' => $path
+      ];
+
+      $this->model->insert('tb_jabatan', $array);
+    }
+
+
+    $this->response(['message' => $cek_data, 'status' => true], 200);
+  }
+}
